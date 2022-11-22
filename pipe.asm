e40d6979beb2830d1c405adcaea2650effeeb007
gbdngb12 <gbdngb12@naver.com>
2022-11-22 14:03:47 +0900

    merge : merge pipe with thread

--- pipe.asm
+++ pipe.asm
@@ -2,13 +2,47 @@ BITS 64
 
 %include "inc64.inc"
 
+;; sys/syscall.h
+%define SYS_write	1
+%define SYS_mmap	9
+%define SYS_clone	56
+%define SYS_exit	60
+
+;; unistd.h
+%define STDIN		0
+%define STDOUT		1
+%define STDERR		2
+
+;; sched.h
+%define CLONE_VM	0x00000100
+%define CLONE_FS	0x00000200
+%define CLONE_FILES	0x00000400
+%define CLONE_SIGHAND	0x00000800
+%define CLONE_PARENT	0x00008000
+%define CLONE_THREAD	0x00010000
+%define CLONE_IO	0x80000000
+
+;; sys/mman.h
+%define MAP_GROWSDOWN	0x0100
+%define MAP_ANONYMOUS	0x0020
+%define MAP_PRIVATE	0x0002
+%define PROT_READ	0x1
+%define PROT_WRITE	0x2
+%define PROT_EXEC	0x4
+
+%define THREAD_FLAGS \
+ CLONE_VM|CLONE_FS|CLONE_FILES|CLONE_SIGHAND|CLONE_PARENT|CLONE_THREAD|CLONE_IO
+
+;%define STACK_SIZE	(4096 * 1024)
+%define STACK_SIZE	(100 * 4)
+
 section .text
     global _start
 
 _start:
     push rbp
     mov rbp, rsp
-    sub rsp, 0x110 ; 148 byte Stack
+    sub rsp, 0x111 ; 148 byte Stack
 
     ;               $rdi
     ; int pipe(int pipefd[2]);
@@ -166,15 +200,39 @@ _parent:
     jne _closeFailed
 
 
+    ;mov edi, dword[rbp + fd1.write]
+    ;mov rsi, lscommand
+    ;mov rdx, 4
+    ;mov rax, SYS_WRITE
+    ;syscall
+    ;running a thread
+    mov rdi, _threadRead
+    call thread_create
+
+inputLoop:
+    ;
+    ; ssize_t read(int fd, void *buf, size_t count);
+    ;
+    mov rdi, STDIN_FILENO
+    lea rsi, [rbp + buffer.buf]
+    mov rdx, 99
+    mov rax, SYS_READ
+    syscall
+    cmp eax, -1
+    je _readFailed
+
+    ;jmp _closeRoutine
+
     mov edi, dword[rbp + fd1.write]
-    mov rsi, lscommand
-    mov rdx, 4
+    lea rsi, [rbp + buffer.buf]
+    mov rdx, rax ;count
     mov rax, SYS_WRITE
     syscall
-    jmp _threadRead
+    jmp inputLoop ;Main Thread wait
 
 
-    
+
+
 _threadWrite:
     ;ssize_t write(int fd, const void *buf, size_t count);
     ;
@@ -182,7 +240,7 @@ _threadWrite:
     ; On success, the number of bytes written is returned.  On error,
     ; -1 is returned, and errno is set to indicate the error.
     mov rdi, STDOUT_FILENO
-    lea rsi, [rbp + buffer.buf]
+    lea rsi, [rbp + c.char]
     mov rdx, 1
     mov rax, SYS_WRITE
     syscall
@@ -199,7 +257,7 @@ _threadRead:
     ;   On error, -1 is returned, and errno is set appropriately.  In this case, it is left un‐
     ;   specified whether the file position (if any) changes.
     mov edi, dword[rbp + fd2.read]
-    lea rsi, [rbp + buffer.buf]
+    lea rsi, [rbp + c.char]
     mov rdx, 1
     mov rax, SYS_READ
     syscall
@@ -214,7 +272,7 @@ _threadRead:
     ; input!
 
     
-
+_closeRoutine:
     ;
     ; int close(int fd);
     ;
@@ -253,6 +311,72 @@ _threadRead:
     exit
 
 
+
+;; long thread_create(void (*)(void))
+thread_create:
+    ;                          $rdi             $rsi           
+	; long clone(unsigned long flags, void *child_stack,
+	;			  $rdx        $r10					$r8
+    ;       void *ptid, void *ctid, struct pt_regs *regs);
+	; pointer is always indicate Low Address
+    ;  ┌────────────────┐            
+	;  │ 32	 33	 34	 35 │
+	;  │ 28	 29	 30	 31 │
+	;  │ 24	 25	 26	 27 │
+	;  │ 20	 21	 22	 23 │
+	;  │ 16	 17	 18	 19 │
+	;  │ 12	 13	 14	 15 │
+	;  │ 8   9	 10	 11 │
+	;  │ 4   5   6   7  │
+	;  │ 0 	 1	 2	 3  │
+	;  └────────────────┘
+	;
+	;				 
+	;            				 ┌────────────────┐						       
+	;         Main Thread rsp--> │    original    │	                           Main Thread ret -> original return Address
+	;      	  					 │ return Address │			
+	;      		 				 ┈ ┈ ┈ ┈ ┈ ┈ ┈ 						      
+	;            				 │    thread_fn   │          sys_clone()  
+	;            				 │     Address    │                       
+	;      		 				 ┈ ┈ ┈ ┈ ┈ ┈ ┈ 						     
+	;    		 				 │................│                     
+	;         New Thread rsp-->  │    thread_fn   │						      New Thread ret -> thread_fn Address
+	;     Stack Size = 12        │     Address    │		 				 
+	; $rax(End Point Address)--> │ 8   9   10  11 │					     
+	;         				     │ 4   5   6   7  │                      
+	;         				     │ 0   1   2   3  │                      
+	;         				     └────────────────┘                            
+
+	push rdi ;thread_fn
+	call stack_create
+	lea rsi, [rax + STACK_SIZE - 8] ;thread function Pointer
+	pop qword [rsi] ; thread function Pointer->thread_fn
+	mov rdi, THREAD_FLAGS ; flags
+	mov rax, SYS_clone
+	syscall ;여기서 thread 생성되며 실행이 계속됨
+	ret
+
+;; void *stack_create(void)
+stack_create:
+    ;                  $rdi          $rsi       $rdx      $r10
+    ; void *mmap(void *addr, size_t length, int prot, int flags,
+    ;                  $r8          $r9
+    ;              int fd, off_t offset);
+    ; Return Value:
+    ; On success, mmap() returns a pointer to the mapped area.  On
+    ;   error, the value MAP_FAILED (that is, (void *) -1) is returned,
+    ;   and errno is set to indicate the error.
+	mov rdi, 0
+	mov rsi, STACK_SIZE
+	mov rdx, PROT_WRITE | PROT_READ
+	mov r10, MAP_ANONYMOUS | MAP_PRIVATE | MAP_GROWSDOWN
+	mov r8, -1
+	mov r9, 0
+	mov rax, SYS_mmap
+	syscall
+	ret
+
+
 ; Data Structure
 struc fd1, -0x8 ;8 Last Size + Current Size
     .read:    resb 4  ;4bytes
@@ -276,15 +400,18 @@ struc buffer, -0xf8; 248 Last Size + Current Size
     .buf: resb 100 ; 100bytes
 endstruc
 
-struc newargv, 0x108;264 Last Size + Current Size
+struc newargv, -0x108;264 Last Size + Current Size
     .sh: resb 8   ;8bytes
     .null: resb 8 ;8bytes
 endstruc
 
-struc newenviron, 0x110;272 Last Size + Current Size
+struc newenviron, -0x110;272 Last Size + Current Size
     .null: resb 8 ;8bytes
 endstruc
 
+struc c, -0x111
+    .char: resb 1;1bytes
+endstruc
 
 ; Error Handling Routine
 _pipeFailed:
@@ -326,6 +453,8 @@ _readFailed:
     pop rbp
     exit
 
+
+
 ; Error Message
 closeError: db"close Error",33,10,0
 pipeError: db "pipe Error",33,10,0
@@ -336,5 +465,4 @@ writeError: db "write Error",33,10,0
 readError: db "read Error",33,10,0
 testString: db "test String",0
 binsh: db "/bin/sh",0
-lscommand: db "ls",0,10
 P_PID equ 1
